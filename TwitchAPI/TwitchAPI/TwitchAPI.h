2bdb770d1947ed61e235604586e6a6a443631687
Maurice Meyer <maurice@polymountain.com>
2014-03-27 00:47:02 +0100

    Added xCode Project

---
+++ TwitchAPI/TwitchAPI/TwitchAPI.h
@@ -0,0 +1,13 @@
+//
+//  TwitchAPI.h
+//  TwitchAPI
+//
+//  Created by PolyNerd on 27/03/14.
+//  Copyright (c) 2014 PolyMountain. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface TwitchAPI : NSObject
+
+@end
---
+++ TwitchAPI/TwitchAPI/TwitchAPI.m
@@ -0,0 +1,13 @@
+//
+//  TwitchAPI.m
+//  TwitchAPI
+//
+//  Created by PolyNerd on 27/03/14.
+//  Copyright (c) 2014 PolyMountain. All rights reserved.
+//
+
+#import "TwitchAPI.h"
+
+@implementation TwitchAPI
+
+@end
