46ec906d1c7b6468e67bb53b4ba40ad5bcf0c05a
Anil Anar <anilanar@hotmail.com>
2014-11-10 20:44:53 +0200

    Fixed JSON mapping

--- Pod/Classes/TwitchAPI.m
+++ Pod/Classes/TwitchAPI.m
@@ -42,10 +42,10 @@
   [[MNetworkJSONRequest JSONRequest:[NSURLRequest requestWithURL:[NSURL URLWithString:url]]
                               success:^(NSURLRequest *request, NSHTTPURLResponse *response, NSDictionary *json)
     {
-        NSArray *channelsAsDict = [[json objectForKey:@"follows"] objectForKey:@"channel"];
+        NSArray *channelItems = [json objectForKey:@"follows"];
         NSMutableArray *channels = [[NSMutableArray alloc] init];
-        for(int i = 0; i < channelsAsDict.count; ++i) {
-            [channels addObject: [[TChannel alloc] initWithDictionary:channelsAsDict[i]]];
+        for(int i = 0; i < channelItems.count; ++i) {
+            [channels addObject: [[TChannel alloc] initWithDictionary:[channelItems[i] objectForKey:@"channel"]]];
         }
         
         if(runOnMainThread) {
